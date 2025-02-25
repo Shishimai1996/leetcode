39e3948b409459df69db7fc78cfd52540a3679ef
Mai Shimizu/舞 清水 <159525515+Shishimai1996@users.noreply.github.com>
2025-02-25 10:56:56 +0900

    another answer

--- 27RemoveElement/index.ts
+++ 27RemoveElement/index.ts
@@ -1,19 +1,40 @@
+// function removeElement(nums: Array<number>, val: number): number {
+//   if (nums.length === 0) return 0;
+//   let k = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== val) {
+//       nums[k] = nums[i];
+//       k++;
+//     }
+//   }
+
+//   return k;
+// }
+
+// const numberArray = [3, 2, 2, 3];
+// const val = 3;
+// console.log(removeElement(numberArray, val));
+// console.log(numberArray);
+
 function removeElement(nums: Array<number>, val: number): number {
   if (nums.length === 0) return 0;
-  let k = 0;
+  let countVal = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      nums[k] = nums[i];
-      k++;
+    if (nums[i] == val) {
+      countVal++;
+    }
+    if (nums[i] != val && countVal > 0) {
+      nums[i - countVal] = nums[i];
     }
   }
-
-  return k;
+  // for (let i = nums.length - 1; i >= nums.length - countVal; i--) {
+  //   nums[i] = "_";
+  // }
+  return nums.length - countVal;
 }
 
-// const numberArray = ["a", "b", "c", "c", "e", "z", "c", "d"];
-// const val = "c";
 const numberArray = [3, 2, 2, 3];
 const val = 3;
+
 console.log(removeElement(numberArray, val));
 console.log(numberArray);
