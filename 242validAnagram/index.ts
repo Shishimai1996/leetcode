0ffe7ed00f4625e22ed2dc36fec342d98b93245c
清水舞 <s.amandamy2161@gmail.com>
2025-04-20 22:13:48 +0900

    add new

---
+++ 242validAnagram/index.ts
@@ -0,0 +1,40 @@
+// function isAnagram(s: string, t: string): boolean {
+//     if(s.length !== t.length) return false
+
+//     let tArray = t.split('')
+
+//     for(let i = 0; i<s.length; i++){
+//         const index = tArray.indexOf(s[i])
+//         if(index !== -1){
+//             tArray.splice(index,1)
+//         }else{
+//             return false
+//         }
+//     }
+//     return tArray.length === 0
+
+// };
+
+// // const sLetter = "anagram"
+// // const tLetter = "nagaram"
+// const sLetter = "rat"
+// const tLetter = "car"
+
+// console.log(isAnagram(sLetter,tLetter))
+
+function isAnagram(s: string, t: string): boolean {
+    if(s.length !== t.length) return false
+
+ const sSort = s.split('').sort().join('')
+ const tSort = t.split('').sort().join('')
+
+ return sSort=== tSort
+
+};
+
+const sLetter = "anagram"
+const tLetter = "nagaram"
+// const sLetter = "rat"
+// const tLetter = "car"
+
+console.log(isAnagram(sLetter,tLetter))
